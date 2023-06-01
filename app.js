530f7133530842b557036ee75e08c5a6ee68f521
Achmad Rivaldi <33930946+mrtampan@users.noreply.github.com>
2023-06-01 08:06:30 +0000

    starto commit

---
+++ app.js
@@ -0,0 +1,50 @@
+async function runningApp() {
+    const env = require('dotenv').config();
+  
+    // Initialization and Authentication
+    const Spotify = require('spotifydl-core').default; // Import the library
+    const spotify = new Spotify({
+      // Authentication
+  
+      clientId: env.parsed.CLIENT_ID_SPOTIFY, // <-- add your own clientId
+      clientSecret: env.parsed.CLIENT_SECRET_SPOTIFY, // <-- add your own clientSecret
+    });
+  
+    // Declaring the respective url in 'links' object
+    const links = {
+      artist: "7n2Ycct7Beij7Dj7meI4X0", // Url of the artist you want to gather info about
+      album: "7hzP5i7StxYG4StECA0rrJ", // Url of the album you want to gather info about
+      song: "4OtVQ2ZxS7yigIjGz5yKg1", // Url of the song you want to gather info about or download
+    };
+    const fs = require('fs-extra');
+  
+    // Engine
+  
+    let songname = '';
+    const album = await spotify.getAlbum(links.album);
+
+
+    album.tracks.forEach(async item => {
+      const data = await spotify.getTrack(item); // Waiting for the data 🥱
+      console.log('Downloading: ', data.name, 'by:', data.artists.join(' ')); // Keep an eye on the progress
+      const song = await spotify.downloadTrack(item); // Downloading goes brr brr
+      fs.writeFileSync('music/' + data.name + '.mp3', song); // Let's write the buffer to the woofer (i mean file, hehehe)
+      console.log('Berhasil Download: ', data.name);
+      songname = 'music/' + data.name + '.mp3';
+    
+      // Save data
+      let musicData = fs.readFileSync('musiclist.json');
+    
+      musicData = JSON.parse(musicData);
+      musicData.music.push({ name: data.name, file: songname });
+    
+      fs.writeFile('musiclist.json', JSON.stringify(musicData), function (error) {
+        console.log("Written file 'musiclist.json'... ");
+      });
+      // end save data
+    
+      console.log(songname);      
+    });
+
+  }
+  runningApp();

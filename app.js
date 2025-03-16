5522dfe71b7610ae2f36988050ecae0643967908
achmadrivaldi <mrtampan54@gmail.com>
2025-03-17 01:04:44 +0700

    Update for support stream

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const fs = require('fs-extra');
 
 async function runningApp() {
@@ -8,52 +8,36 @@ async function runningApp() {
   let title = '';
   let musicData = await fs.readFile('musiclist.json');
 
-  execPromise('spotdl save ' + url + ' --save-file ./spotdl_data/data.spotdl')
-    .then(() => {
-      musicMeta = fs.readFileSync('./spotdl_data/data.spotdl');
-      musicMeta = JSON.parse(musicMeta);
-      for (let i = 0; i < musicMeta[0].artists.length; i++) {
-        title += musicMeta[0].artists[i];
-        title += i == musicMeta[0].artists.length - 1 ? '' : ', ';
-      }
-      title += ' - ' + musicMeta[0].name;
-    })
-    .catch((err) => console.log(err));
+  const spotdlmeta = spawn('spotdl', [
+    'save',
+    url,
+    '--save-file',
+    './spotdl_data/data.spotdl',
+  ]);
+  spotdlmeta.on('close', (code) => {
+    console.log(`Proses title meta selesai dengan kode: ${code}`);
+    musicMeta = fs.readFileSync('./spotdl_data/data.spotdl');
+    musicMeta = JSON.parse(musicMeta);
+    for (let i = 0; i < musicMeta[0].artists.length; i++) {
+      title += musicMeta[0].artists[i];
+      title += i == musicMeta[0].artists.length - 1 ? '' : ', ';
+    }
+    title += ' - ' + musicMeta[0].name;
+  });
 
-  execPromise('spotdl ' + url + ' --output ./music/' + title + '.mp3')
-    .then(() => {
-      // waiting download
-      setTimeout(() => {
-        console.log('done');
-      }, 20000);
-
-      if (!fs.existsSync('./music/' + title + '.mp3')) {
-        process.exit(1); // Menutup program dengan status 1 (error)
-      }
-      musicData = JSON.parse(musicData);
-      musicData.music.push({
-        name: title,
-        source: './music/' + title + '.mp3',
-      });
-
-      fs.writeFileSync('musiclist.json', JSON.stringify(musicData));
-    })
-    .catch((err) => console.log(err));
-}
-
-function execPromise(command) {
-  return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        reject(`Error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        reject(`stderr: ${stderr}`);
-        return;
-      }
-      resolve(stdout); // Mengembalikan hasil dari stdout
+  const spotdlfile = spawn('spotdl', [url, '--output', './music']);
+  spotdlfile.on('close', (code) => {
+    console.log(`Proses download file selesai dengan kode: ${code}`);
+    if (!fs.existsSync('./music/' + title + '.mp3')) {
+      process.exit(1); // Menutup program dengan status 1 (error)
+    }
+    musicData = JSON.parse(musicData);
+    musicData.music.push({
+      name: title,
+      source: './music/' + title + '.mp3',
     });
+
+    fs.writeFileSync('musiclist.json', JSON.stringify(musicData));
   });
 }
 

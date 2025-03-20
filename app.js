9334840d208df5449d3fa8b80785c1d319415788
achmadrivaldi <mrtampan54@gmail.com>
2025-03-21 02:51:27 +0700

    Update process download

--- app.js
+++ app.js
@@ -1,49 +1,31 @@
-const { spawn } = require('child_process');
 const fs = require('fs-extra');
+const { exec } = require('child_process');
 
 async function runningApp() {
-  const url = process.argv[2];
-
   let musicMeta = '';
   let title = '';
   let musicData = await fs.readFile('musiclist.json');
 
-  const spotdlmeta = spawn('spotdl', [
-    'save',
-    url,
-    '--save-file',
-    './spotdl_data/data.spotdl',
-  ]);
-  spotdlmeta.on('close', (code) => {
-    console.log(`Proses title meta selesai dengan kode: ${code}`);
-    musicMeta = fs.readFileSync('./spotdl_data/data.spotdl');
-    musicMeta = JSON.parse(musicMeta);
-    for (let i = 0; i < musicMeta[0].artists.length; i++) {
-      title += musicMeta[0].artists[i];
-      title += i == musicMeta[0].artists.length - 1 ? '' : ', ';
-    }
-    title += ' - ' + musicMeta[0].name;
+  musicMeta = fs.readFileSync('./spotdl_data/data.spotdl');
+  musicMeta = JSON.parse(musicMeta);
+  for (let i = 0; i < musicMeta[0].artists.length; i++) {
+    title += musicMeta[0].artists[i];
+    title += i == musicMeta[0].artists.length - 1 ? '' : ', ';
+  }
+  title += ' - ' + musicMeta[0].name;
+
+  if (!fs.existsSync('./music/' + title + '.mp3')) {
+    process.exit(1); // Menutup program dengan status 1 (error)
+  }
+  musicData = JSON.parse(musicData);
+  musicData.music.push({
+    name: title,
+    source: './music/' + title + '.mp3',
   });
 
-  const spotdlfile = spawn('spotdl', [url, '--output', './music']);
-  spotdlfile.on('close', (code) => {
-    console.log(`Proses download file selesai dengan kode: ${code}`);
-    if (!fs.existsSync('./music/' + title + '.mp3')) {
-      setTimeout(() => {
-        console.log('tunggu');
-      }, 6000);
-    }
-    if (!fs.existsSync('./music/' + title + '.mp3')) {
-      process.exit(1); // Menutup program dengan status 1 (error)
-    }
-    musicData = JSON.parse(musicData);
-    musicData.music.push({
-      name: title,
-      source: './music/' + title + '.mp3',
-    });
+  fs.writeFileSync('musiclist.json', JSON.stringify(musicData));
 
-    fs.writeFileSync('musiclist.json', JSON.stringify(musicData));
-  });
+  exec('rm -rf ./spotdl_data/data.spotdl');
 }
 
 runningApp();
